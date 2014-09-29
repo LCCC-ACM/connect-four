e850207e202044fa8a793946e8ede4ed1fb1f9fa
Garrett Openshaw <garrett.j.openshaw@gmail.com>
2014-09-29 19:12:32 -0400

    Added ant build - moved files.

--- source/Main.java
+++ source/Main.java
@@ -1,6 +1,6 @@
 import gameEngine.*;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
 		GameResult result = new ConnectFourGame().play();
 		System.out.println(result);
